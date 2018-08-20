7a7e535b91dec1b4a43607b5780fc15cbdd6afe3
Ilya Romanenko <mirakulus2017@yandex.ru>
2018-08-20 22:50:00 +0300

    рефакторинг, примитивный поиск по маске

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,6 @@
 import paramiko
-
+import fnmatch
+import os
 LOGS_DIR = '/var/logs/techops'
 
 class AuthData():
@@ -12,33 +13,48 @@ class AuthData():
         else:
             raise Exception('No auth data for server: {0}'.format(server))
 
-def print_200_lines_of(server, log_file_mask, line_id):
+def ssh_connect(server, port):
+    auth_data = AuthData(server)
+
+    ssh = paramiko.client.SSHClient()
+    ssh = paramiko.client.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(server, 2222, auth_data.user, auth_data.password)
+
+    return ssh
+
+def sftp_search(sftp, log_file_mask):
+    ''' Поиск файлов по принятым в Unix системах wildcard '''
+    files_dir = os.path.dirname(log_file_mask)
+    file_mask = os.path.split(log_file_mask)[-1]
+    files = sftp.listdir(files_dir)
+    # replace - из-за того, что разработка ведется на windows машине, и join вставляет обратные слеши, которые Unix не любит
+    log_files = [ os.path.join(files_dir, file).replace('\\', '/') for file in fnmatch.filter(files, file_mask)]
+    return log_files
+
+def print_200_lines_of(server, port, log_file_mask, line_id):
     ''' Метод для поиска в логе на удаленном сервере строки с заданным числовым идентификатором. Возвращает массив строк -100 + 100 от идентификатора
         server - имя или IP сервера где лежит лог
         log_file_mask - маска пути к лог файлу. Допустимо содержания wildcards. (* - любое количество символов)
         line-id - уникальный ID для строки в файле, по которому будет производится поиск
     '''
     # все в одном методе, чтобы точнее понять пути обобщения
-    print('Request authentication for: {0}'.format(server))
-    auth_data = AuthData(server)
-    ssh = paramiko.client.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(server, 2222, auth_data.user, auth_data.password)
-    sftp = ssh.open_sftp()
-    # logfile = [ log for log in sftp.listdir(LOGS_DIR) if log.contains('data')][0]
-    for line in sftp.listdir('/var/log/techops'):
-        print(line)
-    log_file = sftp.open('/var/log/techops/data_01.log')
+    ssh = None
+    sftp = None
     try:
-        for line in log_file:
-            print(line)
+        ssh = ssh_connect(server, port)
+        sftp = ssh.open_sftp()
+        files = sftp_search(sftp, log_file_mask)
+        for file in files:
+            print('open: {0}'.format(file))
+            with sftp.open(file, 'r') as f:
+                for line in f:
+                    print(line)
     finally:
-        log_file.close()
-
-    sftp.close()
-    ssh.close
+        sftp.close()
+        ssh.close()
 
 
 if __name__ == '__main__':
-    print_200_lines_of('192.168.1.5', 'data*.log', 4)
+    print_200_lines_of('192.168.1.5', 2222, '/var/log/techops/data*.log', 4)
 
