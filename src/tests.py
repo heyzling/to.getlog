562420826050ce7fcac65e486e7ed7d4ab51ff83
Ilya Romanenko <mirakulus2017@yandex.ru>
2018-08-21 20:30:08 +0300

    simple test for sftp_search

--- src/tests.py
+++ src/tests.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 # import parser
 import loggetter
+import unittest
 import os
 
-class TestSuite():
+SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+TESTDATA_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, '..', 'testdata'))
 
-    def test_sft_search_file(self):
-        files = loggetter.sftp_search(os, 'c:/Users/MurMi/git*')
-        print(files)
+class TestSuite(unittest.TestCase):
+
+    def test_sft_search_file_only_last_file_mask(self):
+        search_mask = os.path.join(TESTDATA_DIR, 'logs_1', '*.log')
+        files = loggetter.sftp_search(os, search_mask)
+        self.assertEqual(len(files), 3, 'поиск не нашел все файлы логов')
 
 if __name__ == '__main__':
-    TestSuite().test_sft_search_file()
+    TestSuite().test_sft_search_file_only_last_file_mask()
