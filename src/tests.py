bac458071ba216f9c46265ca3618ab76eef164a3
Ilya Romanenko <mirakulus2017@yandex.ru>
2018-08-21 13:09:09 +0300

    proof of concept testing sftp search method locally using os module as sftp connection

--- src/tests.py
+++ src/tests.py
@@ -1,9 +1,13 @@
-import pytest
+# -*- coding: utf-8 -*-
+# import parser
+import loggetter
+import os
 
 class TestSuite():
 
-    def test_one():
-        pass
+    def test_sft_search_file(self):
+        files = loggetter.sftp_search(os, 'c:/Users/MurMi/git*')
+        print(files)
 
 if __name__ == '__main__':
-    pass
+    TestSuite().test_sft_search_file()
