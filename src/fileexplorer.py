b3ac5694c980bda237f40a7233aedc6301525c48
Ilya Romanenko <mirakulus2017@yandex.ru>
2018-08-23 12:52:47 +0300

    FileExplorer.read() + рефакторинг

--- src/fileexplorer.py
+++ src/fileexplorer.py
@@ -1,45 +1,88 @@
 
 class FileExplorer():
-    ''' Расширение чтения файлов. '''
+    ''' Построчное чтение файлов. Возможно обращение к произвольной строке. 
+    Если строка уже была прочитана/итерирована, то переход к ней будет осуществляться быстро,
+    т.к. информация об офсетах строк кешируется '''
 
-    # TODO: собственный file-like object который может оборачиваться вокруг существующего и 
-    # и предоставлять расширенный функционал, вроде кеширвоания оффсетов строк, чтения строки по номер и т.д.
-    # доп. преимущество - использование with в клиентском коде
-
-    def __init__(self, file_object_open_method, file_path, open_mode, encoding):
+    def __init__(self, file_object_open_method, file_path, open_mode):
         ''' 
         file_object_open_method - метод, который создает file-like объект
         file_path - путь к файлу
-        open_mode - метод открытия (r, w, rb, a и так далее)
-        encoding - кодировка файла '''
+        open_mode - метод открытия (r, w, rb, a и так далее). 
+                    От метода открытия зависит тип возвращаемых строк.
+                    Например, при открытии в режиме rb, будет возвращена строка в виде массива байт'''
         self._file_open_method = file_object_open_method
         self._open_mode = open_mode
         self.file_path = file_path
-        self.encoding = encoding
         self._file = self._file_open_method(self.file_path, self._open_mode)
-        self._line_offsets = []
-        self._current_offset = 0
+        self._file_enum = enumerate(self._file)
+        self._line_offsets = [ ]
+        self._cur_offset = 0
+        self._cur_line_index = 0
 
     def __enter__(self):
         return self
-    
     def __exit__(self, type, value, traceback):
         self.close()
 
-    def close(self):
-        self._file.close()
+    def __iter__(self):
+        return self
+    def __next__(self):
+        line_index, line = self._file_enum.__next__()
+        self._line_offsets.append(self._cur_offset)
+        self._cur_offset += len(line)
+        self._cur_line_index = line_index + 1
+        return line
+
+    def _seek_offset(self, offset):
+        ''' устанавливает курсор на указанную позицию в файле '''
+        self._cur_offset = offset
+        return self._file.seek(offset, 0)
+
 
     @property
-    def current_line(self):
-        ''' номер строки на которой стоит курсор '''
-        return len(self._line_offsets)
+    def cur_line_index(self):
+        ''' номер строки в начале которой стоит курсор (0-based) '''
+        return self._cur_line_index
 
     @property
-    def current_offset(self):
-        ''' текущий оффсет курсора, на котором проихсодит чтение '''
-        return self._current_offset
+    def cur_offset(self):
+        ''' текущий оффсет с начала файла на котором стоит курсор '''
+        return self._cur_offset
 
-    def seek(self, string):
+
+    def seek(self, line_index):
+        ''' устанавилвает курсор на начало указанную строку. 
+        Если файл уже читался и оффсет строки закеширован чтение файла заново не происходит '''
+        if len(self._line_offsets) > line_index:
+            self._seek_offset(self._line_offsets[line_index])
+        else:
+            for i in range(len(self._line_offsets), line_index):
+                self.__next__()
+
+    def read(self, lines_amount=1, start_pos=-1):
+        ''' построчно читает файл, начиная с указанной позиции. Возвращает прочитанные строки
+        lines_amount - количество строк, которое нужно прочесть. Если < 0, Будет проитана одна строка
+        start_pos - позиция в файле откуда начинать чтение. По умолчанию (-1) - начинает с текущей позиции
+        return - если прочитана одна строка - вернет строку. Если более - вернет массив строк '''
+        if lines_amount < 0:
+            lines_amount = 1
+
+        if start_pos != -1:
+            raise NotImplementedError
+
+        if lines_amount == 1:
+            return self.__next__()
+        else:
+            start_pos = self.cur_line_index
+            end_pos = start_pos + lines_amount
+            lines = []
+            while self.cur_line_index < end_pos:
+                lines.append(self.__next__())
+
+            return lines
+
+    def search_for(self, string):
         ''' Поиск вхождения указанной строки в файле и остановка курсора на линии с ней.
         returns boolean - найдено ли вхождение или нет  '''
         bstring = string.encode(self.encoding)
@@ -47,39 +90,6 @@ class FileExplorer():
             if bstring in l:
                 return True
         return False
-
-    def read(self):
-        ''' построчно читает файл, возвращает содержание строки '''
-        for i, line in enumerate(self._file, self.current_line):
-            self._line_offsets.append(self._current_offset)
-            self._current_offset += len(line)
-            yield line
-
-# import os
-# import sftp
-# import tools
-
-# SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
-# TESTDATA_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, '..', 'testdata'))
-# DATA_01_LOG = os.path.join(TESTDATA_DIR, 'data_01.log')
-# REMOTE_TESTDATA_DIR = '/var/log/techops'
-# REMOTE_DATA_01_LOG = tools.join_unix(REMOTE_TESTDATA_DIR, 'data_01.log')
-
-# def connect_test_sftp():
-#     server = '192.168.1.5'
-#     port = 2222
-#     return sftp.Sftp(server, port, sftp.AuthData.get_auth_data(server))
-
-# sftp = connect_test_sftp()
-
-# # file = open(DATA_01_LOG, 'rb')
-# # with open(DATA_01_LOG, 'rb') as file:
-# # with FileExplorer(sftp.open, REMOTE_DATA_01_LOG, 'rb', 'utf-8') as f:
-# with FileExplorer(open, DATA_01_LOG, 'rb', 'utf-8') as f:
-#     # fe = FileExplorer(file, 'utf-8')
-#     for l in f.read():
-#         print('{0} -> {1}'.format(f.current_line, l))
-
-#     print(' ----------------- end RREAD -------')
-#     for l in f.read():
-#         print('{0} -> {1}'.format(f.current_line, l))
+    
+    def close(self):
+        self._file.close()
--- src/test_fileexplorer.py
+++ src/test_fileexplorer.py
@@ -6,28 +6,48 @@ from fileexplorer import FileExplorer
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 TESTDATA_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, '..', 'testdata'))
 DATA_01_LOG = os.path.join(TESTDATA_DIR, 'data_01.log')
-
+ENCODING = 'utf-8'
 
 class TestFileExplorer(unittest.TestCase):
 
     def setUp(self):
-        self.fe = FileExplorer(open, DATA_01_LOG, 'rb', 'utf-8')
+        self.fe = FileExplorer(open, DATA_01_LOG, 'rb')
 
     def tearDown(self):
         self.fe.close()
 
-    def test_seek_save_line_index(self):
-        ''' seek - находит указанное вхождение в файле '''
-        id_to_find = '774038205'
-        is_found = self.fe.seek(id_to_find)
-        self.assertTrue(is_found, 'строка с ID={0} не найдена'.format(id_to_find))
+    def test_cur_line_index_iterates_from_0(self):
+        ''' cur_line_index итерируется правильно '''
+        index = 0
+        for l in self.fe:
+            index += 1
+            self.assertEqual(self.fe.cur_line_index, index)
+    
+    def test_read_1_line_return_one_line(self):
+        ''' чтение одной строки'''
+        line = self.fe.read()
+        self.assertEqual(line.rstrip().decode(ENCODING), '20:10:29.085040 185130259 dqhl gzytdvpprvflrivlle h    i')
+        self.assertEqual(self.fe.cur_line_index, 1, '')
 
-    def test_current_line_is_correcto_after_succsessfull_seek(self):
-        ''' Свойство current_line содержит правильный номер строки, после того, как успешно был выполнен seek '''
-        id_to_find = '774038205'
-        is_found = self.fe.seek(id_to_find)
-        self.assertTrue(is_found, 'строка с ID={0} не найдена'.format(id_to_find))
-        self.assertEqual(self.fe.current_line, 77, 'Строка с ID={0} найдено, но свойство current_line содержит неверный номер строки'.format(id_to_find))
+
+    def test_read_n_lines(self):
+        ''' read возвращает указанное количество строк. ''' 
+        lines_to_read = 5
+        lines = self.fe.read(lines_to_read)
+        self.assertEqual(len(lines), lines_to_read, )
+        self.assertEqual(lines[0].rstrip().decode(ENCODING), '20:10:29.085040 185130259 dqhl gzytdvpprvflrivlle h    i', 'Первая прочитанная строка не равна строке из файла')
+        self.assertEqual(lines[lines_to_read - 1].rstrip().decode(ENCODING), '20:10:29.085040 258262132 vzkpk ivvdtpsxlcffehbibvgmjkvq', '{0} строка не равна строке из файла'.format(lines_to_read))
+        self.assertEqual(self.fe.cur_line_index, lines_to_read, '')
+
+    def test_seek_line_return_correct_line(self):
+        ''' seek - должен переходить к указанной строке в файле '''
+        line_to_seek = 21
+        self.fe.seek(line_to_seek)
+        self.assertEqual(self.fe.cur_line_index, 21, 'cur_line_index неправильный')
+    
+    def test_seek_return_to_previous_line(self):
+        ''' возвращение на уже прочитанную линию '''
+        raise NotImplementedError
 
 if __name__ == '__main__':
     unittest.main()
